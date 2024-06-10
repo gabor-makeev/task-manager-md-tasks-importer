f12dcde16af8698124f1ecec0fb8b1dc9cb23f48
Gabor Makeev <gabor.makejev@gmail.com>
2024-06-10 22:02:09 +0200

    develop a simple md checklist importer

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+$my_notes = file_get_contents($argv[1]);
+
+$taskNamePattern = "/^- (\[ ]|\[x]).+/m";
+$taskDescriptionPattern = "/^\s+- (\[ ]|\[x]).+/m";
+
+$my_notes = explode(PHP_EOL, $my_notes);
+
+foreach ($my_notes as $line) {
+  if (!$line) {
+    continue;
+  }
+
+  if (preg_match($taskNamePattern, $line, $nameMatches)) {
+    $taskName = preg_replace("/^- (\[ ]|\[x])\s{1}/", "", $nameMatches[0]);
+    $statusId = 3;
+
+    if (preg_match("/^- \[x]/", $line, $statusClosedMatches)) {
+        $statusId = 4;
+    }
+
+    $array[] = [
+        "name" => $taskName,
+        "statusId" => $statusId
+    ];
+  };
+
+  $previousLine = &$array[count($array) - 1];
+
+  if (preg_match($taskDescriptionPattern, $line, $descriptionMatches) && $previousLine) {
+    $description = preg_replace("/^\s*/", "", $descriptionMatches[0]);
+    isset($previousLine["description"])
+      ? $previousLine["description"] .= PHP_EOL . $description
+      : $previousLine["description"] = $description;
+  }
+}
+
+$mysqli = mysqli_connect('127.0.0.1', 'sail', 'password', 'laravel', 3306);
+
+ foreach($array as $item) {
+     $name = $item['name'];
+     $description = isset($item['description']) ? $item['description'] : "";
+     $userId = 2;
+     $statusId = $item['statusId'];
+
+     $mysqli->query("INSERT INTO tasks (name, description, user_id, status_id) VALUES ('{$name}', '{$description}', '{$userId}', '{$statusId}')");
+ }
